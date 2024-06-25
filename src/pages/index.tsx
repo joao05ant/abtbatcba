9e616cd7421eefddbbdefd6ecc31304e683dcaae
joao05ant <joaoantoniomoliveira@gmail.com>
2024-06-25 16:36:26 -0400

    Adicionando Maps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-// index.tsx
-
 import Header from "../components/header";
 import Image from "next/image";
 
@@ -10,9 +8,8 @@ export default function Home() {
 
       {/* Conteúdo principal da página */}
       <main className="flex flex-col items-center justify-center w-full max-w-screen-lg px-4">
-        <h1 className="text-3xl md:text-5xl font-bold text-center mb-8">
-          Aqui você encontrará algumas informações sobre as top batalhas de rima
-          de Cuiabá
+        <h1 className="text-3xl md:text-5xl text-center mb-8">
+          Aqui você encontrará algumas informações sobre as batalhas de rima de Cuiabá
         </h1>
 
         {/* Exemplo de uso da fonte em um parágrafo */}
@@ -20,7 +17,7 @@ export default function Home() {
           Isso aqui é só um teste
         </p>
 
-        {/* Exemplo de seção */}
+        {/* Seção 1 */}
         <section className="my-16 w-full">
           <div className="max-w-3xl mx-auto">
             <h2 className="text-3xl font-bold mb-4">Batalha da Alencastro</h2>
@@ -34,37 +31,25 @@ export default function Home() {
               />
             </div>
             <p className="mt-4 text-lg text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              aliquet justo ut efficitur. Fusce eu erat nunc. Donec id dapibus
-              augue, ac scelerisque sem.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet justo ut efficitur. Fusce eu erat nunc. Donec id dapibus augue, ac scelerisque sem.
             </p>
-          </div>
-
-          
-        </section> 
-        <section className="my-16 w-full">
-          <div className="max-w-3xl mx-auto">
-            <h3 className="text-3xl font-bold mb-4">Batalha da Alencastro</h3>
-            <div className="flex items-center justify-center">
-              <Image
-                src="/bda.jpg" // Caminho da Img Segundo local
-                alt="Descrição da imagem 1"
-                width={600}
-                height={400}
-                className="rounded-lg"
-              />
+            <div className="flex justify-center mt-4">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31670.23679989548!2d-56.0853158!3d-15.6014104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x939d2be4be5275e7%3A0x9e1d52e7f3e14b71!2sPra%C3%A7a%20Alencastro%2C%20Cuiab%C3%A1%20-%20MT%2C%2078005-000!5e0!3m2!1spt-BR!2sbr!4v1629729058265!5m2!1spt-BR!2sbr"
+                width="300"
+                height="200"
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
-            <p className="mt-4 text-lg text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              aliquet justo ut efficitur. Fusce eu erat nunc. Donec id dapibus
-              augue, ac scelerisque sem.
-            </p>
           </div>
         </section>
       </main>
 
       {/* Rodapé da página */}
-      <footer className="w-full h-24 flex items-center justify-center  text-white">
+      <footer className="w-full h-24 flex items-center justify-center text-white">
         <p>© 2024 Meu Site. Todos os direitos reservados.</p>
       </footer>
     </div>
